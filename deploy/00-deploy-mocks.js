1c2683d1e25a5da999ea0256f18db7e9a18709c2
Junxi Wang <768650397@qq.com>
2023-03-11 23:59:48 +0800

    feat(core): add some

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,13 @@
+const { network } = require("hardhat")
+const { devChains } = require("../helper-hardhat-config")
+
+module.exports = async function ({ deployments, getNamedAccounts }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const chainId = network.config.chainId
+    if (devChains.includes(network.name)) {
+        log("Local network detected! Deploying mocks...")
+        // deploy a mock vfrCoordinator...
+    }
+}
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,5 +1,13 @@
 const networkConfig = {
     5: {
         name: "goerli",
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
     },
+}
+
+const devChains = ["localhost", "hardhat"]
+
+module.exports = {
+    networkConfig,
+    devChains,
 }
