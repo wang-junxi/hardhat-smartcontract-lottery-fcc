abb2a9632067e2e609ac71a1710d541812aa66fe
Junxi Wang <768650397@qq.com>
2023-03-16 22:48:37 +0800

    feat(core): update

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -19,7 +19,7 @@ module.exports = async function ({ deployments, getNamedAccounts }) {
         })
 
         log("Mocks deployed!")
-        log("-----------------------------")
+        log("--------------------------")
     }
 }
 
